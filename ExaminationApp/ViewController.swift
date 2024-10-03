ff574cfc1f1cf33e9d88ecc74cee65c15de14e7d
Aroslav Kochkin <kochkin.2328@inbox.ru>
2024-10-03 20:52:19 +0700

    Удалил лишние отступы

---
+++ ExaminationApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ExaminationApp
+//
+//  Created by Ярослав Кочкин on 03.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
+
